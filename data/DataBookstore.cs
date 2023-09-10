32ba934be8bbd6967265268073d35cadad075865
M153 <wanchatgame@gmail.com>
2023-09-10 18:55:50 +0700

    Upload Data Sqlite

---
+++ data/DataBookstore.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWpfFinal_Bookstore.data
+{
+    internal class DataBookstore
+    {
+        public async static void InitializeDataBookstore()
+        {
+            using (SqliteConnection dbBooks = new SqliteConnection($"Filename=Bookstore.db"))
+            {
+                dbBooks.Open();
+                String tableBooks = "CREATE TABLE IF NOT EXISTS Books(" +
+                    "ISBN INTEGER PRIMARY KEY," +
+                    "Title TEXT NOT NULL," +
+                    "Description TEXT NOT NULL," +
+                    "Price REAL NOT NULL)";
+
+                SqliteCommand createTableBooks = new SqliteCommand(tableBooks, dbBooks);
+                createTableBooks.ExecuteReader();
+                dbBooks.Close();
+            }
+        }
+
+        public static void AddDataBook(int ISBNBook, string TitleBook, string DescriptionBook, string PriceBook)
+        {
+            using (SqliteConnection dbBooks = new SqliteConnection($"Filename=Bookstore.db"))
+            {
+                dbBooks.Open();
+                SqliteCommand insertBooks = new SqliteCommand();
+                insertBooks.Connection = dbBooks;
+
+                insertBooks.CommandText = "INSERT INTO Books VALUES (@ISBN, @Title, @Description, @Price);";
+                insertBooks.Parameters.AddWithValue("@ISBN", ISBNBook);
+                insertBooks.Parameters.AddWithValue("@Title", TitleBook);
+                insertBooks.Parameters.AddWithValue("@Description", DescriptionBook);
+                insertBooks.Parameters.AddWithValue("@Price", PriceBook);
+                insertBooks.ExecuteReader();
+                dbBooks.Close();
+            }
+        }
+
+        public static void UpdateDataBook(int ISBNBook, string TitleBook, string DescriptionBook, string PriceBook)
+        {
+            using (SqliteConnection dbBooks = new SqliteConnection($"Filename=Bookstore.db"))
+            {
+                dbBooks.Open();
+                SqliteCommand updateBooks = new SqliteCommand();
+                updateBooks.Connection = dbBooks;
+
+                updateBooks.CommandText = "UPDATE Books SET Title = @Title, Description = @Description, Price = @Price WHERE ISBN = @ISBN;";
+                updateBooks.Parameters.AddWithValue("@ISBN", ISBNBook);
+                updateBooks.Parameters.AddWithValue("@Title", TitleBook);
+                updateBooks.Parameters.AddWithValue("@Description", DescriptionBook);
+                updateBooks.Parameters.AddWithValue("@Price", PriceBook);
+                updateBooks.ExecuteNonQuery();
+                dbBooks.Close();
+            }
+        }
+
+        public static List<string> GetISBNs()
+        {
+            List<string> isbns = new List<string>();
+
+            using (SqliteConnection db = new SqliteConnection($"Filename=Bookstore.db"))
+            {
+                db.Open();
+                SqliteCommand getISBNs = new SqliteCommand("SELECT ISBN FROM Books", db);
+                using (SqliteDataReader reader = getISBNs.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        isbns.Add(reader["ISBN"].ToString());
+                    }
+                }
+                db.Close();
+            }
+
+            return isbns;
+        }
+    }
+}
---
+++ data/DataCustomers.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWpfFinal_Bookstore.data
+{
+    internal class DataCustomers
+    {
+        public async static void InitializeDataCustomers()
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename=Bookstore.db"))
+            {
+                db.Open();
+                String tableCustomers = "CREATE TABLE IF NOT EXISTS Customers(" +
+                    "Customer_id INTEGER PRIMARY KEY," +
+                    "Customer_Name TEXT NOT NULL," +
+                    "Address TEXT NOT NULL," +
+                    "Email TEXT NOT NULL)";
+
+                SqliteCommand createTableCustomers = new SqliteCommand(tableCustomers, db);
+                createTableCustomers.ExecuteReader();
+                db.Close();
+            }
+        }
+
+        public static void AddDataCustomers(int IdCustomers, string NameCustomers, string AddressCustomers, string EmailCustomers)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename=Bookstore.db"))
+            {
+                db.Open();
+                SqliteCommand insertCustomers = new SqliteCommand();
+                insertCustomers.Connection = db;
+
+                insertCustomers.CommandText = "INSERT INTO Customers VALUES (@Customer_id, @Customer_Name, @Address, @Email);";
+                insertCustomers.Parameters.AddWithValue("@Customer_id", IdCustomers);
+                insertCustomers.Parameters.AddWithValue("@Customer_Name", NameCustomers);
+                insertCustomers.Parameters.AddWithValue("@Address", AddressCustomers);
+                insertCustomers.Parameters.AddWithValue("@Email", EmailCustomers);
+                insertCustomers.ExecuteReader();
+                db.Close();
+            }
+        }
+
+        public static void UpdateDataCustomers(int IdCustomers, string NameCustomers, string AddressCustomers, string EmailCustomers)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename=Bookstore.db"))
+            {
+                db.Open();
+                SqliteCommand updateCustomers = new SqliteCommand();
+                updateCustomers.Connection = db;
+
+                updateCustomers.CommandText = "UPDATE Customers SET Customer_Name = @Customer_Name, Address = @Address, Email = @Email WHERE Customer_id = @Customer_id;";
+                updateCustomers.Parameters.AddWithValue("@Customer_id", IdCustomers);
+                updateCustomers.Parameters.AddWithValue("@Customer_Name", NameCustomers);
+                updateCustomers.Parameters.AddWithValue("@Address", AddressCustomers);
+                updateCustomers.Parameters.AddWithValue("@Email", EmailCustomers);
+                updateCustomers.ExecuteNonQuery();
+                db.Close();
+            }
+        }
+
+        public static List<string> GetCustomerIDs()
+        {
+            List<string> customerIDs = new List<string>();
+
+            using (SqliteConnection db = new SqliteConnection($"Filename=Bookstore.db"))
+            {
+                db.Open();
+                SqliteCommand getCustomerIDs = new SqliteCommand("SELECT Customer_id FROM Customers", db);
+                using (SqliteDataReader reader = getCustomerIDs.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        customerIDs.Add(reader["Customer_id"].ToString());
+                    }
+                }
+                db.Close();
+            }
+
+            return customerIDs;
+        }
+    }
+}
